64c97838b2a3e6a15b75eb512de604db20cae538
Brandon Skewes <brandon.skewes@outlook.com>
2025-05-03 12:44:50 +1000

    made the player move

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
 
+    myclock = pygame.time.Clock()
+
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player_1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player_1.update(dt)
         pygame.Surface.fill(screen, (0, 0, 0))
+        player_1.draw(screen)
+        amt_t = myclock.tick(60)
+
+        dt = amt_t / 1000
+
         pygame.display.flip()
 
 
